b37580b566d9d7f7b8c3798f0b0cf80740ba41c4
Olivia Forbault <olivia.forbault@outlook.fr>
2021-02-17 16:12:34 +0100

    progress exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # get '/restaurants/:id/reviews/new', to: 'restaurants#'
+  # post 'restaurants/:id/reviews', to: 'restaurants#'
+  resources :restaurants do
+    resources :reviews
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+sushi_samba = { name: "Sushi Samba", address: "8 Dam, Amsterdam 1011LB", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+five_guys = { name: "Five Guys", address: "434 Meet, Rotterdam 4930KL", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+madame_jeannette =  { name: "Madame Jeannette", address: "65 Coolsingel Rotterdam 2782", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+
+[ dishoom, pizza_east, sushi_samba, five_guys, madame_jeannette].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
